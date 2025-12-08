227fb9b2c26a37bad302ce9a38a0d7b4b43a00f5
Emin Palaz <eminpalaz01@hotmail.com>
2025-12-08 18:32:07 +0300

    admin buttons fixed

--- src/components/admin/AdminPage.tsx
+++ src/components/admin/AdminPage.tsx
@@ -601,21 +601,21 @@ export function AdminPage({ onLogout }: AdminPageProps) {
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-slate-400">
                       {new Date(post.createdAt).toLocaleDateString() || ''}
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                      <div className="flex space-x-2">
+                    <td className="text-sm font-medium">
+                      <div className="flex">
                         <Button 
                           size="sm" 
                           variant="outline"
                           onClick={() => setViewingBlogPost(post)}
                         >
-                          <Eye className="h-4 w-4" />
+                          <Eye className="h-5 w-5" />
                         </Button>
                         <Button 
                           size="sm" 
                           variant="outline"
                           onClick={() => setEditingBlogPost(post)}
                         >
-                          <Edit className="h-4 w-4" />
+                          <Edit className="h-5 w-5" />
                         </Button>
                         <Button 
                           size="sm" 
@@ -623,10 +623,11 @@ export function AdminPage({ onLogout }: AdminPageProps) {
                           className="text-red-600 hover:text-red-700"
                           onClick={() => handleDeleteBlogPost(post.id)}
                         >
-                          <Trash2 className="h-4 w-4" />
+                          <Trash2 className="h-5 w-5" />
                         </Button>
                       </div>
                     </td>
+
                   </tr>
                 ))}
               </tbody>
